5c1946a2f2cee79bd6996d78e509e51697426b75
leva37a <leva37a@yandex.ru>
2023-03-15 14:38:58 +0300

    Added sign up and sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
     user_params = params.require(:session)
     user = User.find_by(email: user_params[:email])
@@ -12,6 +15,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  def new
-  end
+  def destroy
+  session.delete(:user_id)
+
+  redirect_to root_path, notice: 'Вы вышли из аккаунта'
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/new'
   root to: "questions#index"
 
   resources :users, only: %i[new create]
-  resource :session, only: %i[new create]
+  resource :session, only: %i[new create destroy]
 
   resources :questions do
     put :hide, on: :member
