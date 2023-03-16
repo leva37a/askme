7b85509fc5c9b5e669d6232c7bdde3edbca2c942
leva37a <leva37a@yandex.ru>
2023-03-16 15:07:30 +0300

    Added editing< deleting of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  @user = User.find(params[:id])
+
+  if @user.update(user_params)
+    redirect_to root_path, notice: 'Данные пользователя обновлены'
+  else
+    flash.now[:alert] = 'При попытке сохранить пользователя возникли ошибки'
+    render :edit
+  end
+end
+
+  def destroy
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: "Пользователь удален"
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "questions#index"
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update destroy]
   resource :session, only: %i[new create destroy]
 
   resources :questions do
