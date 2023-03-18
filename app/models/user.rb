31d7c1164de1cdfbdd8f37cd3a250cfec8a1d17f
leva37a <leva37a@yandex.ru>
2023-03-18 08:34:55 +0300

    Editing app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,22 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_save :downcase_nickname
+  before_validation :downcase_nickname, :downcase_email
 
-  validates :nickname, uniqueness: true, length: {maximum: 40},
-                       format: { with: /\A[a-zA-Z\d_]+\z/ }
+  validates :nickname, presence: true, uniqueness: true, length: {maximum: 40},
+                       format: { with: /\A\w+\z/ }
   validates :email, presence: true, uniqueness: true,
-                    format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/ }
+                    format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+
+  has_many :questions, dependent: :delete_all
+
+  private
 
   def downcase_nickname
-    nickname.downcase!
+    nickname&.downcase!
+  end
+
+  def downcase_email
+    email&.downcase!
   end
 end
