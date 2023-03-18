8f1ff372616f5e62a8645d45e3518a227b6f5afc
leva37a <leva37a@yandex.ru>
2023-03-18 09:21:20 +0300

    Added unique index for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_114159) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_18_061437) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_114159) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
 end
